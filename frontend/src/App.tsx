28003d19e34f71e9eab741344e33382b2f402eb9
Jason Mansour <jason.mansour@gwdg.de>
2024-06-16 18:08:35 +0200

    Clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,83 +1,12 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import RuleTable from './RuleTable'
-import { FilterSet, Rule } from './schema'
+import { useEffect, useState } from 'react';
+import './App.css';
+import RuleTable from './RuleTable';
+import { FilterSet, Rule } from './schema';
 
-
-// const initialRules = [
-//   {
-//     "id": 1,
-//     "url_pattern": "https://example.com/blog/...",
-//     "matches": 344,
-//     "include": "Yes",
-//     "type": "Article"
-//   },
-//   {
-//     "id": 2,
-//     "url_pattern": "https://example.com/videos/...",
-//     "matches": 123,
-//     "include": "Yes",
-//     "type": "(Pointer to) Video"
-//   },
-//   {
-//     "id": 3,
-//     "url_pattern": "https://example.com/videos/about",
-//     "matches": 1,
-//     "include": "No",
-//     "type": "-"
-//   }
-// ]
-
-
-const filterSetExample = {
-      "id": 2,
-      "crawl_job": {
-          "id": 6,
-          "start_url": "https://weltderphysik.de",
-          "follow_links": true,
-          "created_at": "2024-06-12T11:41:37.056465Z",
-          "updated_at": "2024-06-12T11:41:37.058759Z"
-      },
-      "name": "Test",
-      "created_at": "2024-06-14T08:29:50.221698Z",
-      "updated_at": "2024-06-14T08:29:50.221712Z",
-      "url": "http://127.0.0.1:8000/filter_sets/2/",
-      "rules": [
-          {
-              "id": 3,
-              "rule": "Blah",
-              "include": false,
-              "created_at": "2024-06-14T09:49:18.166863Z",
-              "updated_at": "2024-06-14T09:49:18.166927Z",
-              "page_type": "Blub",
-              "count": 100
-          },
-          {
-              "id": 4,
-              "rule": "Blah",
-              "include": false,
-              "created_at": "2024-06-14T09:55:31.738095Z",
-              "updated_at": "2024-06-14T09:55:31.738136Z",
-              "page_type": "Blub",
-              "count": 100
-          },
-          {
-              "id": 5,
-              "rule": "https://example.com/videos/...",
-              "include": true,
-              "created_at": "2024-06-14T09:55:57.020011Z",
-              "updated_at": "2024-06-14T09:55:57.020128Z",
-              "page_type": "New row",
-              "count": 123
-          }
-      ]
-  };
 
 function App() {
-  let [filterSet, setFilterSet] = useState<FilterSet|null>(null);
-  let [rules, setRules] = useState<Rule[]>([]);
+  const [filterSet, setFilterSet] = useState<FilterSet | null>(null);
+  const [rules, setRules] = useState<Rule[]>([]);
   // fetch data from http://127.0.0.1:8000/filter_sets/1/
 
   async function fetchData() {
@@ -95,41 +24,19 @@ function App() {
 
   async function deleteRow(id: number) {
     console.log("delete", id);
-    
+
     const response = await fetch(`http://127.0.0.1:8000/filter_rules/${id}/`, {
       method: 'DELETE',
     });
     const data = await response.json();
     console.log(data);
 
-    //rules = rules.filter((rule) => rule.id !== id);
     setRules(rules.filter((rule) => rule.id !== id));
   }
 
-  // function addRowAfter(id: number) {
-  //   console.log("add after", id);
-  //   let newRule = {
-  //     "id": 4,
-  //     "url_pattern": "https://example.com/videos/...",
-  //     "matches": 123,
-  //     "include": "Yes",
-  //     "type": "New row"
-  //   }
-  //   //setRules([...rules, newRule]);
-  //   // insert after rule with the id
-  //   let newRules = [];
-  //   for (let rule of rules) {
-  //     newRules.push(rule);
-  //     if (rule.id === id) {
-  //       newRules.push(newRule);
-  //     }
-  //   }
-  //   setRules(newRules);
-  // }
-
   async function addRowAfter(id: number) {
     console.log("add after", id);
-    let newRule = {
+    const newRule = {
       // TODO: how to make it so we can use an ID and not a URL?
       "filter_set": "http://127.0.0.1:8000/filter_sets/1/",
       //"filter_set": "1",
@@ -165,7 +72,7 @@ function App() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({rule: newRuleString}),
+      body: JSON.stringify({ rule: newRuleString }),
     });
     const updatedRule = await response.json();
     console.log(updatedRule);
@@ -191,7 +98,7 @@ function App() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({position: old_position+delta}),
+        body: JSON.stringify({ position: old_position + delta }),
       });
       const updatedRule = await response.json();
       console.log(updatedRule);
@@ -208,11 +115,11 @@ function App() {
       <p>604 pages total, 5 not handled yet</p>
       <h3>Rules</h3>
       <RuleTable rules={rules}
-      onDelete={deleteRow}
-      onAdd={addRowAfter}
-      onUpdate={(id, newRule) => {updateRow(id, newRule); console.log("onChange", id, newRule);}}
-      onMoveUp={moveDelta(-1)}
-      onMoveDown={moveDelta(1)}
+        onDelete={deleteRow}
+        onAdd={addRowAfter}
+        onUpdate={(id, newRule) => { updateRow(id, newRule); console.log("onChange", id, newRule); }}
+        onMoveUp={moveDelta(-1)}
+        onMoveDown={moveDelta(1)}
       />
 
       <div>
@@ -220,34 +127,6 @@ function App() {
       </div>
     </div>
   );
-
-
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + Reacts</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
 }
 
 export default App
--- frontend/src/RuleTable.tsx
+++ frontend/src/RuleTable.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import { Rule } from './schema';
 
 // A component that shows some text. When clicked, the text becomes editable.
 // If you press enter or click outside, it accepts. If you press ESC, it cancels.
-function Editable(props: {value: string, onChange: (value: string) => void}) {
+function Editable(props: { value: string, onChange: (value: string) => void }) {
   const [editing, setEditing] = useState(false);
   const [value, setValue] = useState(props.value);
 
@@ -18,7 +18,7 @@ function Editable(props: {value: string, onChange: (value: string) => void}) {
     }
   }
 
-  return <span onClick={() => {setEditing(true); setValue(props.value);}}>
+  return <span onClick={() => { setEditing(true); setValue(props.value); }}>
     {props.value}
     {editing ? <input value={value} onChange={(e) => setValue(e.target.value)} onKeyDown={handleKeyDown} autoFocus onBlur={() => setEditing(false)} /> : ""}
   </span>
@@ -30,38 +30,38 @@ type RuleTableProps = {
   onAdd: (id: number) => void,
   onUpdate: (id: number, rule: string) => void,
   onMoveUp: (id: number) => void,
-  onMoveDown: (id: number) => void,  
+  onMoveDown: (id: number) => void,
 }
 
 
 export default function RuleTable(props: RuleTableProps) {
-    return <table className="table">
-      <thead>
-          <tr>
-            <th>URL Pattern</th>
-            <th>Matches</th>
-            <th>Include</th>
-            <th>Type</th>
-            {/* <th>Position</th> */}
-            <th></th>
-          </tr>
-          </thead>
-          <tbody>
-          {props.rules.map((rule: Rule) => {
-            return <tr key={rule.id}>
-              <td className="editable-cell"><Editable value={rule.rule} onChange={newValue => props.onUpdate(rule.id, newValue)}/></td>
-              <td>{rule.count}</td>
-              <td>{rule.include ? 'Yes' : 'No'}</td>
-              <td>{rule.page_type}</td>
-              {/* <td>{rule.position}</td> */}
-              <td>
-                <button className="mybutton mybutton-table mybutton-delete" onClick={(e) => props.onDelete(rule.id)}>×</button>
-                <button className="mybutton mybutton-table" onClick={(e) => props.onAdd(rule.id)}>+</button>
-                <button className="mybutton mybutton-table" disabled={rule.position == 1} onClick={(e) => props.onMoveUp(rule.id)}>&uarr;</button>
-                <button className="mybutton mybutton-table" disabled={rule.position == props.rules.length} onClick={(e) => props.onMoveDown(rule.id)}>&darr;</button>
-              </td>
-            </tr>
-          })}
-          </tbody>
-        </table>
-  }
+  return <table className="table">
+    <thead>
+      <tr>
+        <th>URL Pattern</th>
+        <th>Matches</th>
+        <th>Include</th>
+        <th>Type</th>
+        {/* <th>Position</th> */}
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.rules.map((rule: Rule) => {
+        return <tr key={rule.id}>
+          <td className="editable-cell"><Editable value={rule.rule} onChange={newValue => props.onUpdate(rule.id, newValue)} /></td>
+          <td>{rule.count}</td>
+          <td>{rule.include ? 'Yes' : 'No'}</td>
+          <td>{rule.page_type}</td>
+          {/* <td>{rule.position}</td> */}
+          <td>
+            <button className="mybutton mybutton-table mybutton-delete" onClick={(e) => props.onDelete(rule.id)}>×</button>
+            <button className="mybutton mybutton-table" onClick={(e) => props.onAdd(rule.id)}>+</button>
+            <button className="mybutton mybutton-table" disabled={rule.position == 1} onClick={(e) => props.onMoveUp(rule.id)}>&uarr;</button>
+            <button className="mybutton mybutton-table" disabled={rule.position == props.rules.length} onClick={(e) => props.onMoveDown(rule.id)}>&darr;</button>
+          </td>
+        </tr>
+      })}
+    </tbody>
+  </table>
+}
