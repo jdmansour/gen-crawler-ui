03ad6b4cf4a4dcfd434db1110636b6758da8b8e3
Jason Mansour <jason.mansour@gwdg.de>
2024-08-09 15:52:38 +0200

    Fix relative css imports in dev mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/static/",
+  server: {
+    origin: 'http://localhost:5173'
+  },
   build: {
     manifest: 'manifest.json',
     outDir: './assets_output',
