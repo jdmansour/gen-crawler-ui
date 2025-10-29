32bf7ff77e935d61b4ca4ff3c7227ffad1e2b9ae
Jason Mansour <jason.mansour@gwdg.de>
2025-10-29 09:53:33 +0100

    Add main SPA

--- frontend/src/wlo_spa.tsx
+++ frontend/src/wlo_spa.tsx
@@ -5,11 +5,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { Timeline } from './Timeline.tsx'
 import './index.css'
-import { Button, MetadataInheritancePage, FilterTabs, TabInfo } from 'wlo-components';
+import { Button, MetadataInheritancePage, FilterTabs, TabInfo, SiteLayout, GenCrawlerSidebar, ShowSidebarButton, DashboardPage,
+SelectSourcePage,
+FilterCrawlsPage } from 'wlo-components';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Breadcrumbs, Breadcrumb } from 'wlo-components';
 import 'wlo-components/styles'; // CSS-Datei importieren
 import { MemoryRouter } from 'react-router-dom';
 
-document.writeln("WOLLOLO");
+// document.writeln("WOLLOLO");
 
 
 const root = document.getElementById('spa_root')!;
@@ -39,21 +44,64 @@ const dashboardTabs: TabInfo[] = [
   { tag: "published", label: "Im Prüfbuffet", icon: 'error' },
 ];
 
-function App() {
-  const [selectedTab, setSelectedTab] = React.useState<number>(0);
+export default function App() {
+  const location = useLocation();
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+
+  const step = location.state?.step || "dashboard";
+
+  const breadcrumbs: Breadcrumb[] = [
+    { label: "Startseite", url: "/" },
+    { label: "Quellen", url: "/" },
+    { label: "Generischer Crawler", url: "/" },
+  ];
+  if (step != "dashboard") {
+    // Add breadcrumb for current crawler
+    breadcrumbs.push({ label: "Neuer Crawler" });
+  }
+
+  useEffect(() => {
+    // Set the document title
+    document.title = `Generic crawler (${step})`;
+  }, [step]);
+
   return (
-    <div>
-    <MemoryRouter initialEntries={['/']}>
-      <FilterTabs tabs={dashboardTabs} selectedTab={selectedTab} onTabClick={setSelectedTab} />
-      <MetadataInheritancePage />
-      <Button default={true}>Click Me</Button>
-    </MemoryRouter>
-    </div>
+    <>
+      <SiteLayout
+        sidebar={<GenCrawlerSidebar step={step} />}
+        sidebarVisible={sidebarVisible}
+        onSidebarClose={() => setSidebarVisible(false)}
+      >
+        {!sidebarVisible && (
+          <ShowSidebarButton onClick={() => setSidebarVisible(true)} />
+        )}
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
+        {step == "dashboard" && (
+          <DashboardPage />
+        )}
+        {step == "select-source" && (
+          <SelectSourcePage />
+        )}
+
+        {step == "metadata-inheritance" && (
+          <MetadataInheritancePage />
+        )}
+        {step == "filter-crawls" && (
+          <FilterCrawlsPage />
+        )}
+      </SiteLayout>
+    </>
   );
+}
+
+function AppWrapper() {
+  return <MemoryRouter initialEntries={['/']}>
+    <App />
+  </MemoryRouter>;
 }
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
 )
