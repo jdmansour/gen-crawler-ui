6a5961b5ccf0a0f2f706012cf6d3083a78bbe9a1
Jason Mansour <jason.mansour@gwdg.de>
2025-11-22 10:24:17 +0100

    Remove unused code

--- frontend-spa/src/index.ts
+++ frontend-spa/src/index.ts
@@ -1,6 +1,5 @@
 // Exportiere nur die essentiellen Komponenten
 export { default as MetadataInheritancePage } from './MetadataInheritancePage';
-export { default as Button } from './Button';
 export { default as Breadcrumbs } from './Breadcrumbs';
 export { CustomIcon } from './CustomIcon';
 export { default as FilterTabs } from './FilterTabs';
@@ -9,7 +8,6 @@ export { default as ListView } from './ListView';
 export { default as Sheet } from './Sheet';
 export { default as GenCrawlerSidebar } from './GenCrawlerSidebar';
 export { default as DashboardPage } from './DashboardPage';
-export { default as FilterCrawlsPage } from './FilterCrawlsPage';
 export { default as SelectSourcePage } from './SelectSourcePage';
 export { default as SiteLayout } from './SiteLayout';
 export { ShowSidebarButton } from './SiteLayout';
